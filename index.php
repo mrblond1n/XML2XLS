fc94952acf1719662a0afeb040cc7c4e9736ff89
Кирдяпин Никита Алексеевич <nalkirdyapin@sberbank.ru>
2020-09-11 13:51:46 +0300

    внесены изменения согласно ТЗ

--- index.php
+++ index.php
@@ -10,190 +10,93 @@ function utf8ucfirst($string)
     return $first . $other;
 }
 
+function getItemValues($content) {
+    $message = '';
+    $count = 0;
+    $orderSum = 0;
+    foreach ($content as $item) {
+        $message .= $item['GoodsCode'] . ' - ' . $item['Count'] . '; ';
+        $count += $item['Count'];
+        $orderSum += $item['PriceWithDiscount'];
+    }
+    return array('Message' => $message, 'Count' => $count, 'OrderSum' => $orderSum);
+}
+
+function getFullAddress($src) {
+    $string = '';
+    $string = gettype($src['Zipcode']) === 'array' ? '' : $src['Zipcode'] . ', ';
+    $src['Street'] && $string .= $src['Street'] . ', ';
+    $src['Home'] && $string .= 'д.' . $src['Home'] . ', ';
+    $src['Building'] && $string .= $src['Building'] . ', ';
+    $src['Flat'] && $string .= 'кв.' . $src['Flat'] . ', ';
+    return $string;
+}
+
+function getFullName($client) {
+    return utf8ucfirst($client['LastName']) . ' ' . utf8ucfirst($client['FirstName']) . ' ' . utf8ucfirst($client['MiddleName']);
+}
+
 require_once __DIR__ . '/config.php';
 
 if (isset($_FILES['file']['error']) && $_FILES['file']['error'] == 0 && substr($_FILES['file']['name'], -4) == '.xml') {
     $xml = json_decode(json_encode(simplexml_load_file($_FILES['file']['tmp_name'])), true);
-
     $codes = array();
-    $f = fopen('codes.txt', 'r');
-    while (!feof($f)) {
-        $s = trim(fgets($f));if ($s == '') {
-            continue;
-        }
-
-        list($code, $price, $kolvo, $mailtype) = explode('|', $s);
-        $codes[$code] = array('price' => $price, 'kolvo' => $kolvo, 'mailtype' => $mailtype);
-    }
-    fclose($f);
-
-    $indexes = array();
-    $f = fopen('indexes.txt', 'r');
-    while (!feof($f)) {
-        $s = trim(fgets($f));if ($s == '') {
-            continue;
-        }
-
-        list($index, $gorod, $region, $codez, $optimize) = explode('|', $s);
-        $indexes[$index]['codes'] = explode(',', $codez);
-        $indexes[$index]['optimize'] = $optimize;
-        $indexes[$index]['gorod'] = $gorod;
-        $indexes[$index]['region'] = $region;
-    }
-    fclose($f);
 
     require_once __DIR__ . '/phpexcel/Classes/PHPExcel.php';
     $phpexcel = new PHPExcel();
     $page = $phpexcel->setActiveSheetIndex(0);
     $page->setCellValue("A1", "ADDRESSLINE");
     $page->setCellValue("B1", "ADRESAT");
-    $page->setCellValue("C1", "MASS");
+    $page->setCellValue("C1", "MASS"); // ВЕС брать из таблицы (товары для акции с почтой)
     $page->setCellValue("D1", "VALUE");
     $page->setCellValue("E1", "PAYMENT");
     $page->setCellValue("F1", "COMMENT");
     $page->setCellValue("G1", "MAILTYPE");
-    $page->setCellValue("H1", "ДОСТАВКА");
-    $page->setCellValue("I1", "ОШИБКА");
-    $page->setCellValue("J1", "МИНИМУМ");
+    $page->setCellValue("H1", "COUNT");
+    $page->setCellValue("I1", "ORDERSTATUS");
+    $page->setCellValue("J1", "ДОСТАВКА");
+    $page->setCellValue("K1", "ИНФОРМАЦИЯ");
 
     foreach ($xml['Order'] as $k => $v) {
         $error = '';
         $highlite = '';
-        $zakaz = $v['ExtID'];
-        $srcaddress = $v['ClientReceiver']['Address'];
-        $address = '';
-        if ($srcaddress['Region']) {
-            $address .= $srcaddress['Region'] . ', ';
-        }
-
-        if ($srcaddress['Area']) {
-            $address .= $srcaddress['Area'] . ', ';
-        }
-
-        if ($srcaddress['City']) {
-            $address .= $srcaddress['City'] . ', ';
-        }
-
-        if ($srcaddress['Street']) {
-            $address .= $srcaddress['Street'] . ', ';
-        }
-
-        if ($srcaddress['Home']) {
-            $address .= 'д.' . $srcaddress['Home'] . ', ';
-        }
-
-        if ($srcaddress['Building']) {
-            $address .= $srcaddress['Building'] . ', ';
-        }
-
-        if ($srcaddress['Flat']) {
-            $address .= 'кв.' . $srcaddress['Flat'] . ', ';
-        }
-
-        $address = mb_substr($address, 0, -2, 'UTF-8');
-
-        $cnt = 0;
-        foreach ($v['Content']['Item'] as $v1) {
+        $orderId = $v['ExtID'];
+        $srcAddress = $v['ClientReceiver']['Address'];
+        $clientReceiver = $v['ClientReceiver'];
+        $address = getFullAddress($srcAddress);
+        $addressant = getFullName($clientReceiver);
+        $deliverySum = $v['OrderDeliverySum'];
+        $orderStatus = $v['OrderStatus'];
+        $content = $v['Content'];
+        $countItems = 0;
+        $mailType = 23;
+        $payment = 0;
+        $mass = '–';
+        foreach ($content['Item'] as $v1) {
             if (is_array($v1)) {
-                $cnt++;
-            }
-
-        }
-        if ($cnt > 0) {
-            $error .= " \r\nНеверно указано количество товара";
-            $highlite = 'FF0000';
-        } else {
-            $code = $v['Content']['Item']['GoodsCode'];
-            $count = $v['Content']['Item']['Count'];
-        }
-        $ordersum = sprintf('%02.2f', 600 * $count);
-        $deliverysum = $v['OrderDeliverySum'];
-
-        if (!isset($indexes[$srcaddress['Zipcode']])) {
-            $error .= " \r\nHет кодов отправления для данного почтового индекса";
-            $highlite = 'FF0000';
-        } elseif (!in_array($code, $indexes[$srcaddress['Zipcode']]['codes'])) {
-            $error .= " \r\nКод отправления $code не соответствует почтовому индексу, допустимые " . implode(',', $indexes[$srcaddress['Zipcode']]['codes']);
-            $highlite = 'FF0000';
-        } elseif ($count > $codes[$code]['kolvo']) {
-            $error .= " \r\nПревышен максимум товаров (" . $codes[$code]['kolvo'] . ' шт) для кода отправления ' . $code . ', указано ' . $count . ' шт';
-            $highlite = 'FF0000';
-        } elseif ($deliverysum != $codes[$code]['price']) {
-            $error .= " \r\nНеверная сумма доставки для кода отправления $code, должно быть " . $codes[$code]['price'] . ' руб';
-            $highlite = 'FF0000';
-        }
-        $min = $deliverysum;
-        $minstr = '';
-        foreach ($indexes[$srcaddress['Zipcode']]['codes'] as $tempcode) {
-            if ($count <= $codes[$tempcode]['kolvo'] && $codes[$tempcode]['price'] < $min) {
-                $min = $codes[$tempcode]['price'];
-                $minstr = 'Есть возможность отправки дешевле, код отправления ' . $tempcode . ', стоимость ' . $min;
+                $countItems++;
             }
         }
-
-//          if($deliverysum==$codes[$code]['price']) $deliverysum='0.00';
-
-        $mailtype = '';
-        if ($code == 1) {
-            $mailtype = '';
-        }
-
-        $address = $srcaddress['Zipcode'] . ', ' . utf8ucfirst($indexes[$srcaddress['Zipcode']]['gorod']) . ', ';
-        if ($srcaddress['Street']) {
-            $address .= $srcaddress['Street'] . ', ';
-        }
-
-        if ($srcaddress['Home']) {
-            $address .= 'д.' . $srcaddress['Home'] . ', ';
-        }
-
-        if ($srcaddress['Building']) {
-            $address .= $srcaddress['Building'] . ', ';
-        }
-
-        if ($srcaddress['Flat']) {
-            $address .= 'кв.' . $srcaddress['Flat'] . ', ';
-        }
-
-        $address = mb_substr($address, 0, -2, 'UTF-8');
+        $itemValues = $countItems ? getItemValues($content['Item']) : getItemValues($content);  
 
         $page->setCellValue("A" . ($k + 2), $address);
-        $page->setCellValue("B" . ($k + 2), utf8ucfirst($v['ClientReceiver']['LastName']) . ' ' . utf8ucfirst($v['ClientReceiver']['FirstName']) . ' ' . utf8ucfirst($v['ClientReceiver']['MiddleName']));
-        $page->setCellValue("C" . ($k + 2), sprintf('%01.2f', 1.75 * $count));
-        $page->setCellValue("D" . ($k + 2), $ordersum);
-        $page->setCellValue("E" . ($k + 2), 0);
-        $page->setCellValue("F" . ($k + 2), $zakaz);
-        $page->setCellValue("G" . ($k + 2), $codes[$code]['mailtype']);
-        $page->setCellValue("H" . ($k + 2), $deliverysum);
-        $page->setCellValue("I" . ($k + 2), ($error == '' ? $error : substr($error, 3)));
-        $page->setCellValue("J" . ($k + 2), $minstr);
-
-        if ($highlite) {
-            $page->getStyle("A" . ($k + 2))->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setRGB($highlite);
-        }
+        $page->setCellValue("B" . ($k + 2), $addressant);
+        $page->setCellValue("C" . ($k + 2), $mass);
+        $page->setCellValue("D" . ($k + 2), $itemValues['OrderSum']);
+        $page->setCellValue("E" . ($k + 2), $payment);
+        $page->setCellValue("F" . ($k + 2), $orderId);
+        $page->setCellValue("G" . ($k + 2), $mailType);
+        $page->setCellValue("H" . ($k + 2), $itemValues['Count']);
+        $page->setCellValue("I" . ($k + 2), $orderStatus);
+        $page->setCellValue("J" . ($k + 2), $deliverySum);
+        $page->setCellValue("K" . ($k + 2), $itemValues['Message']);
 
     }
-
-    $page->getColumnDimension("A")->setAutoSize(true);
-    $page->getStyle("A1")->getFont()->setBold(true);
-    $page->getColumnDimension("B")->setAutoSize(true);
-    $page->getStyle("B1")->getFont()->setBold(true);
-    $page->getColumnDimension("C")->setAutoSize(true);
-    $page->getStyle("C1")->getFont()->setBold(true);
-    $page->getColumnDimension("D")->setAutoSize(true);
-    $page->getStyle("D1")->getFont()->setBold(true);
-    $page->getColumnDimension("E")->setAutoSize(true);
-    $page->getStyle("E1")->getFont()->setBold(true);
-    $page->getColumnDimension("F")->setAutoSize(true);
-    $page->getStyle("F1")->getFont()->setBold(true);
-    $page->getColumnDimension("G")->setAutoSize(true);
-    $page->getStyle("G1")->getFont()->setBold(true);
-    $page->getColumnDimension("H")->setAutoSize(true);
-    $page->getStyle("H1")->getFont()->setBold(true);
-    $page->getColumnDimension("I")->setAutoSize(true);
-    $page->getStyle("I1")->getFont()->setBold(true);
-    $page->getColumnDimension("J")->setAutoSize(true);
-    $page->getStyle("I1")->getFont()->setBold(true);
+    $arr = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K');
+    foreach ($arr as &$value) {
+      $page->getColumnDimension($value)->setAutoSize(true);
+      $page->getStyle($value . 1)->getFont()->setBold(true);
+    };
 
     header('Content-type: application/vnd.ms-excel');
     header('Content-Disposition: attachment; filename="file.xls"');
@@ -205,50 +108,52 @@ if (isset($_FILES['file']['error']) && $_FILES['file']['error'] == 0 && substr($
 } else {
 
     $content = '
-<div class="hidden-xs hidden-md col-lg-4"></div>
-<div class="col-xs-12 col-md-12 col-lg-4">
-<b style="text-transform:uppercase">' . $row['firstname'] . ' ' . $row['lastname'] . '<br><br></b>
-<form action="" method="POST" enctype="multipart/form-data">
-  <div class="form-group">
-    <label for="file">&nbsp;Загрузка XML файла</label>
-    <input type="file" class="form-control" name="file" id="file" value="" placeholder="Загрузка XML файла">
-  </div>
-  <button type="submit" class="btn btn-success btn-primary btn-lg" style="width:100%">Загрузка XML файла</button>
-</form>
-</div>
-<div class="hidden-xs hidden-md col-lg-4"></div>';
+        <div class="hidden-xs hidden-md col-lg-4"></div>
+        <div class="col-xs-12 col-md-12 col-lg-4">
+            <b style="text-transform:uppercase">' . $row['firstname'] . ' ' . $row['lastname'] . '<br><br></b>
+            <form action="" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="file">&nbsp;Загрузка XML файла</label>
+                    <input type="file" class="form-control" name="file" id="file" value="" placeholder="Загрузка XML файла">
+                </div>
+                <button type="submit" class="btn btn-success btn-primary btn-lg" style="width:100%">Загрузка XML файла</button>
+            </form>
+        </div>
+        <div class="hidden-xs hidden-md col-lg-4"></div>
+    ';
 }
 
 if ($error) {
     $content = '
-<div class="hidden-xs hidden-md col-lg-4"></div>
-<form class="col-xs-12 col-md-12 col-lg-4" action="" method="POST">
-  <div class="form-group">
-    <label for="email">&nbsp;Введите свой e-mail</label>
-    <input type="email" class="form-control" name="email" id="email" value="" placeholder="Введите свой e-mail">
-  </div>
-  <div class="form-group">
-    <label for="password">&nbsp;Введите свой пароль</label>
-    <input type="password" class="form-control" name="password" id="password" value="" placeholder="Введите свой пароль">
-  </div>
-  <button type="submit" class="btn btn-success btn-primary btn-lg" style="width:100%">Войти</button>
-</form>
-<div class="hidden-xs hidden-md col-lg-4"></div>';
+        <div class="hidden-xs hidden-md col-lg-4"></div>
+            <form class="col-xs-12 col-md-12 col-lg-4" action="" method="POST">
+                <div class="form-group">
+                    <label for="email">&nbsp;Введите свой e-mail</label>
+                    <input type="email" class="form-control" name="email" id="email" value="" placeholder="Введите свой e-mail">
+                </div>
+                <div class="form-group">
+                    <label for="password">&nbsp;Введите свой пароль</label>
+                    <input type="password" class="form-control" name="password" id="password" value="" placeholder="Введите свой пароль">
+                </div>
+                <button type="submit" class="btn btn-success btn-primary btn-lg" style="width:100%">Войти</button>
+            </form>
+        <div class="hidden-xs hidden-md col-lg-4"></div>
+    ';
 }
 
 ?>
 <?php header('Content-type: text/html; charset=utf-8');?>
 <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="css/bootstrap.css" rel="stylesheet">
-<script src="js/jquery.modern.js"></script>
-</head>
-<body>
-<div class="container" style="padding:30px">
-<div class="row">
-<?=$content?>
-</div>
-</div>
-</body>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <script src="js/jquery.modern.js"></script>
+    </head>
+    <body>
+        <div class="container" style="padding:30px">
+            <div class="row">
+               <?=$content?>
+            </div>
+        </div>
+    </body>
 </html>
